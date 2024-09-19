a1be9634d31c5464fc7506280e283024bd449f00
fwix92 <128750066+fwix92@users.noreply.github.com>
2024-09-19 16:36:45 +0200

    Add files via upload

---
+++ Projet_BDDA/src/DBConfig.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class DBConfig {
+	String dbpath;
+	
+	public DBConfig (String dbp){
+		this.dbpath = dbp;
+		
+	}
+	
+	public static DBConfig LoadDBConfig(File fichier_config) {
+		
+
+	}
+	
+}
