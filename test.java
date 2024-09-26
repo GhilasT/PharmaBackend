150772382185eabd14fa00496aca7c52b2695f1f
maximel77 <maxime.laville7732@gmail.com>
2024-09-26 16:58:16 +0200

    Update test.java

--- test.java
+++ test.java
@@ -6,6 +6,10 @@ public class test {
     
     File fichier_config = new File("fichier.txt"); 
             DBConfig config = DBConfig.LoadDBConfig(fichier_config);
+            System.out.println(config.dbpath);
+            System.out.println(config.pagesize);
+            System.out.println(config.dm_maxfilesize);
+            
 
 }
 }
