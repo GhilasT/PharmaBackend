adb4bc5081da3f07a64dd55283892b62acc3ee16
maximel77 <maxime.laville7732@gmail.com>
2024-09-26 16:57:37 +0200

    Update DBConfig.java

--- DBConfig.java
+++ DBConfig.java
@@ -1,25 +1,42 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-// commentaire
+
 public class DBConfig {
     String dbpath;
-
-    // ommentaire 2
-    public DBConfig(String dbp) {
+    int pagesize;
+    int dm_maxfilesize;
+    
+    public DBConfig(String dbp,int ps,int dmax) {
         this.dbpath = dbp;
+        this.pagesize=ps;
+        this.dm_maxfilesize=dmax;
     }
-    //cm3
+
     
     public static DBConfig LoadDBConfig(File fichier_config) throws FileNotFoundException {
         DBConfig dbConfig = null;  
         Scanner myReader = new Scanner(fichier_config);
+        String data;
+        int ps;
+        int dmax;
 
        
         if (myReader.hasNextLine()) {
-            String data = myReader.nextLine();
-            dbConfig = new DBConfig(data); 
-			System.out.println(data); 
+            data = myReader.nextLine();
+			
+        
+        	if(myReader.hasNextInt()) {
+        		ps=myReader.nextInt();
+        		
+        		if(myReader.hasNextInt()) {
+        			dmax=myReader.nextInt();
+        			dbConfig=new DBConfig(data,ps,dmax);
+        			
+        		}
+        		
+        	}
+        	
         }
 
         myReader.close();
