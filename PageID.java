266ef20399251f6fe7083c1cebea1f95c069efdf
Imene Bakhouche <imenebakhouche@BIUSPORT-TELE09.bium.univ-paris5.fr>
2024-09-26 17:13:24 +0200

    modif

--- PageID.java
+++ PageID.java
@@ -1,7 +1,29 @@
 public class PageID {
-	int FileIdx;
-	int PageIdx;
+	private int FileIdx;
+	private int PageIdx;
+	public PageID(int fileIdx, int pageIdx) {
+		FileIdx = fileIdx;
+		PageIdx = pageIdx;
+	}
 
-	// cop
+	public int getFileIdx() {
+		return FileIdx;
+	}
+
+	public int getPageIdx() {
+		return PageIdx;
+	}
+
+	public void setFileIdx(int fileIdx) {
+		FileIdx = fileIdx;
+	}
+
+	public void setPageIdx(int pageIdx) {
+		PageIdx = pageIdx;
+	}
+	
+	
+
+	
 
 }
