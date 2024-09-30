cee00f7903cbdedfe0bacaeb1950855ca51fcb0a
fwix92 <128750066+fwix92@users.noreply.github.com>
2024-09-30 11:21:24 +0200

    Delete Projet_bbdda directory

--- Projet_bbdda/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class Main {
-	public static void main(String[]args) throws IOException {
-		DBConfig FichTest = DBConfig.LoadDBConfig("testTP1.json");
-		System.out.println("Dbpath de test : "+ FichTest.getDBpath()+" taille : "+ FichTest.getPagesize()+" taille max "+ FichTest.getDM_maxfilesize());
-	}
-
-}
