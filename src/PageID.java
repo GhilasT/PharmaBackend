0a2dda68c8521834d87cf84d6143306fc631d5dd
Imene Bakhouche <imenebakhouche@MacBook-Air-de-Imene.local>
2024-10-06 16:21:43 +0200

    modifFinale

---
+++ src/PageID.java
@@ -0,0 +1,30 @@
+import java.io.*;
+public class PageID {
+	private int FileIdx;
+	private int PageIdx;
+	public PageID(int fileIdx, int pageIdx) {
+		FileIdx = fileIdx;
+		PageIdx = pageIdx;
+	}
+
+	public int getFileIdx() {
+		return FileIdx;
+	}
+
+	public int getPageIdx() {
+		return PageIdx;
+	}
+
+	public void setFileIdx(int fileIdx) {
+		FileIdx = fileIdx;
+	}
+
+	public void setPageIdx(int pageIdx) {
+		PageIdx = pageIdx;
+	}
+	
+	
+
+	
+
+}
