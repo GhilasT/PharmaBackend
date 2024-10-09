b992277b4651d44596000ecfbb2860c1d6d57a9e
Imene Bakhouche <imenebakhouche@MacBook-Air-de-Imene.local>
2024-10-09 23:14:34 +0200

    Buffer

---
+++ src/Buffer.java
@@ -0,0 +1,63 @@
+public class Buffer {
+    // Identifiant de la page dans ce buffer
+    private PageID pageId;
+    // Les données de la page chargée dans ce buffer
+    private byte[] pageData;
+    // nombre d'utilisations de la page 
+    private int pinCount;
+    // Indique si la page a été modifiée
+    private boolean dirty;
+    // Temps du dernier accès pour LRU
+    private long lastAccessTime;
+
+    public Buffer() {
+        this.pageId = null;
+        this.pageData = null;
+        this.pinCount = 0;
+        this.dirty = false;
+        // initialiser avec le temps courant
+        this.lastAccessTime = System.currentTimeMillis();
+
+    }
+    public PageID getPageId() {
+        return pageId;
+    }
+
+    public void setPageId(PageID pageId) {
+        this.pageId = pageId;
+    }
+
+    public byte[] getPageData() {
+        return pageData;
+    }
+
+    public void setPageData(byte[] pageData) {
+        this.pageData = pageData;
+    }
+
+    
+    public int getPinCount() {
+        return pinCount;
+    }
+
+    public void incrementPinCount() {
+        pinCount++;
+    }
+
+    public void decrementPinCount() {
+        if (pinCount > 0) {
+            pinCount--;
+        }
+    }
+
+
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
+    }
+    public void updateLastAccessTime() {
+    }
+}
--- src/BufferManager.java
+++ src/BufferManager.java
@@ -1,25 +1,60 @@
 import java.nio.ByteBuffer;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class BufferManager {
 	private DBConfig config;
 	private DiskManager disk;
-	private ArrayList<ByteBuffer> buffListe;
-	
-	public BufferManager(DBConfig config, DiskManager disk) {
+	private ArrayList<Buffer> buffListe;
+
+	// ajouter le nombre de buffeur
+	public BufferManager(DBConfig config, DiskManager disk, int nbrBuff) {
 		this.config = config;
 		this.disk = disk;
+		this.buffListe = new ArrayList<>();
+		for (int i = 0; i < nbrBuff; i++) {
+            buffListe.add(new Buffer());
+        }
 	}
 	
 	
-	public ByteBuffer GetPage(PageID pageId) {
-		
-		disk.ReadPage(pageId,buffListe.get(Page));
-		
-	return buffListe.get(?);
+	public ByteBuffer GetPage(PageID pageId) throws Exception {
+		// verfier si la page est déjà dans un buffer
+
+		for (Buffer buffer : buffListe) {
+			if (buffer.getPageId() != null && buffer.getPageId().equals(pageId)) {
+				// incementer le pinCount
+				buffer.incrementPinCount();
+
+				// Met à jour le dernier accès pour LRU
+				buffer.updateLastAccessTime();
+
+				return ByteBuffer.wrap(buffer.getPageData());
+			}
+		}
 	
+		// si la page n'est pas en mémoire => chercher un buffer libre
+		for (Buffer buffer : buffListe) {
+			if (buffer.getPinCount() == 0) {
+
+				ByteBuffer tempBuffer = ByteBuffer.allocate(config.getPagesize());
+				disk.ReadPage(pageId, tempBuffer);
+				// madifier les attributs du buffer
+				buffer.setPageData(tempBuffer.array());
+				buffer.setPageId(pageId);
+				buffer.setDirty(false);
+				buffer.incrementPinCount();
+				buffer.updateLastAccessTime();
+				return ByteBuffer.wrap(buffer.getPageData());
+			}
+		}
+	
+		// si les buffers sont utilisés (aucun avec pin_count == 0)
+		throw new IOException("Pas de buffer libre disponible");
 	}
+
 	
 	public void FreePage(PageID pageId, boolean valdirty) {
 		
