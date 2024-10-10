49122947c8ca08b44ac6d1422f8fcea74245a184
fwix92 <128750066+fwix92@users.noreply.github.com>
2024-10-10 17:38:59 +0200

    Add files via upload

---
+++ src/Record.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Record {
+	ArrayList<String> liste;
+	
+	public Record(ArrayList<String> liste) {
+		this.liste = liste;
+	}
+	
+	/*public ArrayList<String> getListe(){
+		return liste;
+	}*/
+}
---
+++ src/Relation.java
@@ -0,0 +1,18 @@
+import java.nio.Buffer;
+
+public class Relation {
+	String nom;
+	int nbColonnes;
+	String nomColonne;
+	String typeColonne;
+	
+	
+	
+	public int writeRecordToBuffer(Record record, Buffer buff, int pos) {
+		
+	}
+	
+	public int readFromBuffer(Record record, Buffer buff, int pos) {
+		
+	}
+}
