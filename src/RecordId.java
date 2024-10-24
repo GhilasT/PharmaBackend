ade5f25ec8f9e668a625668ba2318cb7e7de506a
fwix92 <128750066+fwix92@users.noreply.github.com>
2024-10-24 17:05:39 +0200

    Update RecordId.java

--- src/RecordId.java
+++ src/RecordId.java
@@ -1,5 +1,17 @@
-
 public class RecordId {
-	PageID pageId;
-	int slotIdx;
+	private PageID pageId;
+	private int slotIdx;
+	
+	public RecordId(PageID pageId,int slotIdx) {
+		this.pageId = pageId;
+		this.slotIdx = slotIdx;
+	}
+	
+	public PageID getPageId() {
+		return pageId;
+	}
+	
+	public int getSlotIdx() {
+		return slotIdx;
+	}
 }
