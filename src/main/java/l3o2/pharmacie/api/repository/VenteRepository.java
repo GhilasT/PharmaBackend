d7adc01dddfe7828d50b120a344024d256dcd81a
Ghilas Tidjet <ghilastidjet7@gmail.com>
2025-04-25 17:28:39 +0200

    Suppression de l'analyse de ventes

--- src/main/java/l3o2/pharmacie/api/repository/VenteRepository.java
+++ src/main/java/l3o2/pharmacie/api/repository/VenteRepository.java
@@ -1,9 +1,12 @@
 package l3o2.pharmacie.api.repository;
 
 import l3o2.pharmacie.api.model.entity.Vente;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.Date;
 
@@ -19,20 +22,35 @@ public interface VenteRepository extends JpaRepository<Vente, UUID> {
      * @param pharmacienAdjointId Identifiant du pharmacien adjoint.
      * @return Liste des ventes effectuées par ce pharmacien adjoint.
      */
+    @EntityGraph(attributePaths = {
+        "pharmacienAdjoint", 
+        "client",
+        "medicamentsPanier",
+        "medicamentsPanier.stockMedicament",
+        "medicamentsPanier.stockMedicament.presentation",
+        "medicamentsPanier.stockMedicament.presentation.cisBdpm"
+    })
     List<Vente> findByPharmacienAdjoint_IdPersonne(UUID pharmacienAdjointId);
-
     /**
      * Recherche les ventes effectuées par un client spécifique.
      * @param clientId Identifiant du client.
      * @return Liste des ventes associées à ce client.
      */
+    @EntityGraph(attributePaths = {
+        "pharmacienAdjoint",
+        "client",
+        "medicamentsPanier.stockMedicament"
+    })
     List<Vente> findByClient_IdPersonne(UUID clientId);
-
 
      // Recherche les ventes effectuées dans une période donnée.
 
+    @EntityGraph(attributePaths = {
+        "pharmacienAdjoint",
+        "client",
+        "medicamentsPanier.stockMedicament"
+    })
     List<Vente> findByDateVenteBetween(Date start, Date end);
-
 
     /**
      * Recherche les ventes par mode de paiement (ex: Espèces, Carte bancaire).
@@ -40,4 +58,24 @@ public interface VenteRepository extends JpaRepository<Vente, UUID> {
      * @return Liste des ventes correspondant au mode de paiement.
      */
     List<Vente> findByModePaiement(String modePaiement);
+    @Override
+    @EntityGraph(attributePaths = {
+        "pharmacienAdjoint",
+        "client",
+        "medicamentsPanier",
+        "medicamentsPanier.stockMedicament",
+        "medicamentsPanier.stockMedicament.presentation",
+        "medicamentsPanier.stockMedicament.presentation.cisBdpm"
+    })
+    List<Vente> findAll();
+    @EntityGraph(attributePaths = {
+        "pharmacienAdjoint",
+        "client",
+        "medicamentsPanier",
+        "medicamentsPanier.stockMedicament.presentation.cisBdpm"
+    })
+    Optional<Vente> findById(UUID idVente);
+
+    
+
 }
